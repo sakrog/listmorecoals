d5e354de93f34656ac767594c297f7ad47039613
Sakib Shaikh <sakibshaikh@Sakibs-MacBook-Air.local>
2015-12-14 15:09:51 -0600

    Migration.php

---
+++ database/migration.php
@@ -0,0 +1,37 @@
+<?PHP
+// migration file for adlister
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'parks_db');
+define('DB_USER', 'parks_user');
+define('DB_PASS', 'password');
+
+require_once 'dbconnect.php';
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$query = 'DROP TABLE IF EXISTS users';
+$dbc->exec($query);
+$query = 'CREATE TABLE users (
+	userid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	first_name VARCHAR(240) NOT NULL,
+	last_name VARCHAR(240) NOT NULL,
+	email VARCHAR(240) NOT NULL UNIQUE,
+	city VARCHAR(240) NOT NULL,
+	state VARCHAR(2) NOT NULL,
+	password VARCHAR(240) NOT NULL,
+	PRIMARY KEY (userid)
+	)';
+
+$query = 'DROP TABLE IF EXISTS posts';
+$dbc->exec($query);
+$query = 'CREATE TABLE posts (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	userid INT UNSIGNED NOT NULL,
+	post_date DATE NOT NULL,
+	title VARCHAR(240) NOT NULL,
+	price INT(240) NOT NULL,
+	description VARCHAR(240) NOT NULL,
+	PRIMARY KEY (id)
+	)';
+
+?>
