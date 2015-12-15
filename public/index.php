6fd97713bc7dda24238598990d76ce30f820f278
pascalallen <thomaspascalallen@yahoo.com>
2015-12-15 13:21:17 -0600

    Adding modal to login.

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- BOOTSTRAP -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!-- BOOTSTRAP CSS -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
 		<!-- CUSTOM CSS -->
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<!-- TITLE IMG -->
@@ -24,6 +24,11 @@
 
 
 		<?php include "../views/partials/footer.php"; ?>
+		<!-- JQUERY -->
+		<script src="/js/jquery-2.1.4.min.js"></script>
+		<!-- BOOTSTRAP JS -->
+		<script src="/js/bootstrap.min.js"></script>
+		<!-- CUSTOM JS -->
 		<script src="js/main.js"></script>
 	</body>
 </html>
