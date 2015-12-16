90a60da380bca0959796207627d0f2d6fba7eeb5
Roger Chin <roger.sc.chin@gmail.com>
2015-12-16 09:58:13 -0600

    Modified Ads.show

---
+++ public/ads.show.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title>Ad Lister</title>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- BOOTSTRAP CSS -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<!-- CUSTOM CSS -->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<!-- TITLE IMG -->
+		<!-- <link rel="shortcut icon" href="img/mole.png"> -->
+	</head>
+	<body>
+		<?php include "../views/partials/navbar.php"; ?>
+		<?php include "../views/partials/header.php"; ?>
+
+		<div class="container">
+			<h1>ITEM FOR SALE <small>Price</small></h1>
+			<div class="description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</div>	
+		</div>	
+
+
+		<?php include "../views/partials/footer.php"; ?>
+		<!-- JQUERY -->
+		<script src="/js/jquery-2.1.4.min.js"></script>
+		<!-- BOOTSTRAP JS -->
+		<script src="/js/bootstrap.min.js"></script>
+		<!-- CUSTOM JS -->
+		<script src="js/main.js"></script>
+	</body>
+</html>
--- views/partials/header.php
+++ views/partials/header.php
@@ -4,6 +4,6 @@
 		<p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
 responsive, mobile-first projects on the web.</p> 
 	</div>
-	<p>This is some text.</p> 
-	<p>This is another text.</p> 
+	<!-- <p>This is some text.</p> 
+	<p>This is another text.</p>  -->
 </div>
