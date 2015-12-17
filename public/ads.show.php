0f60e5d85d04b9c24232453079efe22d2234b7f5
Roger Chin <roger.sc.chin@gmail.com>
2015-12-17 09:55:11 -0600

    Changed ads.show.php

---
+++ public/ads.show.php
@@ -0,0 +1,54 @@
+<?php
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'adlister');
+define('DB_USER', 'vagrant');
+define('DB_PASS', 'vagrant');
+
+$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME .'', DB_USER , DB_PASS);
+
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+require_once '../utils/Input.php';
+
+$query = "SELECT * FROM posts WHERE id=1";
+$stmt = $dbc->prepare($query);
+$title->bindValue(':title', 1,PDO::PARAM_STR);
+
+
+?>
+
+<html>
+	<head>
+		<title>Ad Lister</title>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- BOOTSTRAP CSS -->
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<!-- CUSTOM CSS -->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<!-- TITLE IMG -->
+		<!-- <link rel="shortcut icon" href="img/mole.png"> -->
+	</head>
+	<body>
+		<?php include "../views/partials/navbar.php"; ?>
+		<?php include "../views/partials/header.php"; ?>
+
+		<div class="container">
+			<h1><?= $row['title']; ?><small><?= $post['price']; ?></small></h1>
+			<div class="description">
+				<?= $post['description'];?>
+		</div>	
+
+
+		<?php include "../views/partials/footer.php"; ?>
+		<!-- JQUERY -->
+		<script src="/js/jquery-2.1.4.min.js"></script>
+		<!-- BOOTSTRAP JS -->
+		<script src="/js/bootstrap.min.js"></script>
+		<!-- CUSTOM JS -->
+		<script src="js/main.js"></script>
+	</body>
+</html>
+
